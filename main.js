b8d321cfafa338b93312db305bc4d133db426fdc
Mairamkross <dyikanbaevmairambek@gmail.com>
2023-02-08 12:26:06 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+
+
+
+// function F(){
+//     function f1(){
+//     $('.img1').css({'opacity' : '1' , 'transition' : '1s'})
+//     $('.img6').css({'opacity' : '0' , 'transition' : '1s'})
+// }
+// f1()
+
+// function f2(){
+//     $('.img2').css({'opacity' : '1' , 'transition' : '1s'})
+// }
+// setTimeout(f2 , 1000)
+
+// function f3(){
+//     $('.img3').css({'opacity' : '1' , 'transition' : '1s'})
+//     $('.img1').css({'opacity' : '0' , 'transition' : '1s'})
+// }
+// setTimeout(f3 , 3000)
+
+// function f4(){
+//     $('.img4').css({'opacity' : '1' , 'transition' : '1s'})
+//     $('.img2').css({'opacity' : '0' , 'transition' : '1s'})
+// }
+// setTimeout(f4 , 3500)
+
+// function f5(){
+//     $('.img5').css({'opacity' : '1' , 'transition' : '1s'})
+//     $('.img3').css({'opacity' : '0' , 'transition' : '1s'})
+// }
+// setTimeout(f5 , 2800)
+
+// function f6(){
+//     $('.img6').css({'opacity' : '1' , 'transition' : '1s'})
+//     $('.img4').css({'opacity' : '0' , 'transition' : '1s'})
+// }
+// setTimeout(f6 , 1400)
+
+// function f7(){
+//     $('.img7').css({'opacity' : '1' , 'transition' : '1s'})
+//     $('.img5').css({'opacity' : '0' , 'transition' : '1s'})
+// }
+// setTimeout(f7 , 1900)
+// }
+// setInterval(F , 2000)
+
+
+
+function g1(){
+    $('.car').css({'width' : '60px' , 'height' : '45px' , 'margin-top' : '450px' , 'margin-left' : '685px' , 'transition' : '1s'})
+}
+g1()
